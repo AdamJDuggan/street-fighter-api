102f18bbb1835342ff4727aac2ec87e5fe4d3ea8
Adam Duggan <aduggan@bluecresthealth.com>
2025-03-24 12:26:13 +0000

    adding handlebars

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,22 @@
 import { NestFactory } from "@nestjs/core";
+import { NestExpressApplication } from "@nestjs/platform-express";
 import { AppModule } from "./app.module";
 import { ValidationPipe } from "@nestjs/common";
 import { SwaggerModule, DocumentBuilder } from "@nestjs/swagger";
+import { join } from "path";
+import * as hbs from "express-handlebars";
 
 async function bootstrap() {
-	const app = await NestFactory.create(AppModule);
+	const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+	// app.useStaticAssets(join(__dirname, "..", "public"));
+
+	app.setBaseViewsDir(join(__dirname, "..", "views"));
+
+	app.engine("hbs", hbs({ extname: "hbs" }));
+
+	app.setViewEngine("hbs");
+
 	/**
 	 * Generate OpenAPI documentation for endpoints
 	 */
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+	content: [
+		"./src/**/*.{html,ts}", // Add paths to your NestJS app source files
+		"./public/**/*.html", // Add path to HTML files in the public directory
+	],
+	theme: {
+		extend: {},
+	},
+	plugins: [],
+};
